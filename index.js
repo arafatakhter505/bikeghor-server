37a992c0225c8e4a5d84a3c1ff935e52b3a7d7f0
arafatakhter505 <arafatakhterforhad@gmail.com>
2022-11-24 20:31:15 +0600

    users post api

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, MongoRuntimeError } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 // midleware
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.grcofim.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const categories = client.db("BikeGhor").collection("Categories");
+    const usersCollection = client.db("BikeGhor").collection("Users");
+
+    // create data
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+run().catch((e) => console.log(e));
 
 app.listen(port, () => console.log(`BikeGhor server running is on ${port}`));
