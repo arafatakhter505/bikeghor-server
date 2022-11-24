df10b367e034bf72cab143c3d320bfca48890983
arafatakhter505 <arafatakhterforhad@gmail.com>
2022-11-25 00:05:32 +0600

    categories api

--- index.js
+++ index.js
@@ -16,11 +16,18 @@ async function run() {
   try {
     const categories = client.db("BikeGhor").collection("Categories");
     const usersCollection = client.db("BikeGhor").collection("Users");
+    const productsCollection = client.db("BikeGhor").collection("Products");
 
     // create data
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
       res.send(result);
     });
 
@@ -31,11 +38,19 @@ async function run() {
       const user = await usersCollection.findOne(query);
       res.send({ isBuyer: user?.role === "Buyer" });
     });
+
     app.get("/users/seller/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email };
       const user = await usersCollection.findOne(query);
       res.send({ isSeller: user?.role === "Seller" });
+    });
+
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const cursor = categories.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
     });
   } catch (error) {
     console.log(error);
